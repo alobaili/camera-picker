0baf620c8b25e851581ea3a7039be5f4717a8fa6
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2023-05-12 15:49:13 +0300

    Define `CameraPicker` and `CameraPickerItem`

--- Sources/CameraPicker/CameraPicker.swift
+++ Sources/CameraPicker/CameraPicker.swift
@@ -1,6 +1,51 @@
-public struct CameraPicker {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
+import AVFoundation
 
-    public init() {
+public enum CameraPickerItem {
+    case image(Image)
+    case video(AVPlayer)
+}
+
+extension CameraPickerItem: Identifiable {
+    public var id: UUID {
+        UUID()
+    }
+}
+
+public struct CameraPicker<Label>: View where Label: View {
+    let label: Label
+    @Binding var selection: [CameraPickerItem]
+
+    @State private var showingCamera = false
+
+    public init(
+        selection: Binding<[CameraPickerItem]>,
+        @ViewBuilder label: () -> Label
+    ) {
+        _selection = selection
+        self.label = label()
+    }
+
+    public var body: some View {
+        Button {
+            showingCamera = true
+        } label: {
+            label
+        }
+        .fullScreenCover(isPresented: $showingCamera) {
+            Text("Camera Here")
+
+            Button("Close") {
+                showingCamera = false
+            }
+        }
+    }
+}
+
+struct CameraPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        CameraPicker(selection: .constant([])) {
+            Text("Example Text View Label")
+        }
     }
 }
