8f12ba2e0248b29a6cd3442b319049c8ed1e4ee1
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2023-05-19 22:50:57 +0300

    Add more initializers for convenience

--- Sources/CameraPicker/CameraPicker.swift
+++ Sources/CameraPicker/CameraPicker.swift
@@ -12,7 +12,6 @@ public struct CameraPicker<Label>: View where Label: View {
     let label: Label
     @Binding var selection: [CameraPickerItem]
     @State private var imagePickerControllerError: LocalizedError?
-
     @State private var showingCamera = false
 
     let allowesEditing: Bool
@@ -71,6 +70,24 @@ public struct CameraPicker<Label>: View where Label: View {
 }
 
 extension CameraPicker {
+    private static func arrayBindingFrom(
+        optionalBinding: Binding<CameraPickerItem?>
+    ) -> Binding<[CameraPickerItem]> {
+        Binding<[CameraPickerItem]> {
+            if let item = optionalBinding.wrappedValue {
+                return [item]
+            } else {
+                return []
+            }
+        } set: { newItems in
+            if let item = newItems.first {
+                optionalBinding.wrappedValue = item
+            } else {
+                optionalBinding.wrappedValue = nil
+            }
+        }
+    }
+
     public init(
         selection: Binding<CameraPickerItem?>,
         allowsEditing: Bool = false,
@@ -80,21 +97,101 @@ extension CameraPicker {
         flashMode: UIImagePickerController.CameraFlashMode = .auto,
         @ViewBuilder label: () -> Label
     ) {
-        let binding = Binding<[CameraPickerItem]> {
-            if let item = selection.wrappedValue {
-                return [item]
-            } else {
-                return []
-            }
-        } set: { newItems in
-            if let item = newItems.first {
-                selection.wrappedValue = item
-            } else {
-                selection.wrappedValue = nil
-            }
-        }
+        self.init(
+            selection: Self.arrayBindingFrom(optionalBinding: selection),
+            allowsEditing: allowsEditing,
+            preferredMediaTypes: preferredMediaTypes,
+            cameraDevice: cameraDevice,
+            captureMode: captureMode,
+            flashMode: flashMode,
+            label: label
+        )
+    }
+}
 
-        self.init(selection: binding, allowsEditing: allowsEditing, preferredMediaTypes: preferredMediaTypes, cameraDevice: cameraDevice, captureMode: captureMode, flashMode: flashMode, label: label)
+extension CameraPicker where Label == Text {
+    public init(
+        _ titleKey: LocalizedStringKey,
+        selection: Binding<CameraPickerItem?>,
+        allowsEditing: Bool = false,
+        preferredMediaTypes: Set<CameraPickerMediaType> = [.image],
+        cameraDevice: UIImagePickerController.CameraDevice = .rear,
+        captureMode: UIImagePickerController.CameraCaptureMode = .photo,
+        flashMode: UIImagePickerController.CameraFlashMode = .auto
+    ) {
+        self.init(
+            selection: selection,
+            allowsEditing: allowsEditing,
+            preferredMediaTypes: preferredMediaTypes,
+            cameraDevice: cameraDevice,
+            captureMode: captureMode,
+            flashMode: flashMode
+        ) {
+            Text(titleKey)
+        }
+    }
+
+    public init<S>(
+        _ title: S,
+        selection: Binding<CameraPickerItem?>,
+        allowsEditing: Bool = false,
+        preferredMediaTypes: Set<CameraPickerMediaType> = [.image],
+        cameraDevice: UIImagePickerController.CameraDevice = .rear,
+        captureMode: UIImagePickerController.CameraCaptureMode = .photo,
+        flashMode: UIImagePickerController.CameraFlashMode = .auto
+    ) where S: StringProtocol {
+        self.init(
+            selection: selection,
+            allowsEditing: allowsEditing,
+            preferredMediaTypes: preferredMediaTypes,
+            cameraDevice: cameraDevice,
+            captureMode: captureMode,
+            flashMode: flashMode
+        ) {
+            Text(title)
+        }
+    }
+
+    public init(
+        _ titleKey: LocalizedStringKey,
+        selection: Binding<[CameraPickerItem]>,
+        allowsEditing: Bool = false,
+        preferredMediaTypes: Set<CameraPickerMediaType> = [.image],
+        cameraDevice: UIImagePickerController.CameraDevice = .rear,
+        captureMode: UIImagePickerController.CameraCaptureMode = .photo,
+        flashMode: UIImagePickerController.CameraFlashMode = .auto
+    ) {
+        self.init(
+            selection: selection,
+            allowsEditing: allowsEditing,
+            preferredMediaTypes: preferredMediaTypes,
+            cameraDevice: cameraDevice,
+            captureMode: captureMode,
+            flashMode: flashMode
+        ) {
+            Text(titleKey)
+        }
+    }
+
+    public init<S>(
+        _ title: S,
+        selection: Binding<[CameraPickerItem]>,
+        allowsEditing: Bool = false,
+        preferredMediaTypes: Set<CameraPickerMediaType> = [.image],
+        cameraDevice: UIImagePickerController.CameraDevice = .rear,
+        captureMode: UIImagePickerController.CameraCaptureMode = .photo,
+        flashMode: UIImagePickerController.CameraFlashMode = .auto
+    ) where S: StringProtocol {
+        self.init(
+            selection: selection,
+            allowsEditing: allowsEditing,
+            preferredMediaTypes: preferredMediaTypes,
+            cameraDevice: cameraDevice,
+            captureMode: captureMode,
+            flashMode: flashMode
+        ) {
+            Text(title)
+        }
     }
 }
 
